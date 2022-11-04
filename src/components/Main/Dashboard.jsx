edfe9c4cf5f0420801837a4c2f5bf65aa9fd3d5d
devcodypanda <83539643+devcodypanda@users.noreply.github.com>
2022-11-04 16:07:46 +0530

    side_nav_change

--- src/components/Main/Dashboard.jsx
+++ src/components/Main/Dashboard.jsx
@@ -25,81 +25,71 @@ export default function Dashboard() {
 
   return (
     <div className='row pt-3 ps-4 px-4 main_dashboard'>
-      <div className='side_bar_div col-lg-3 col-md-3 col-12 text-white'>
-        <div className="blur_color border border-light rounded-5">
+      <div className='side_bar_div col-lg-3 col-md-3 col-12'>
+        <div className="blur_color side_main rounded-5">
           <div>
-            <center className="p-4">
+            <center className="pt-4 pb-4">
               <Avatar
                 alt="Remy Sharp"
                 src="/images/slide-1.webp"
                 sx={{ width: "70%", height: "70%" }}
               />
-              <h4 className="mt-3 fw-bold">codypanda</h4>
-              <p className="mt-2 text-light">
-                userID : <span>00000</span>
+              <h4 className="mt-3 fw-bold" style={{color:"#F94C66"}}>codypanda</h4>
+              <p className="mt-2" style={{color:"#F8ED86",fontSize:'110%'}}>
+                userID : <span  style={{color:"#ffff"}}>00000</span>
               </p>
             </center>
-            <hr />
-            <div>
 
-            </div>
-            <div className="px-4 mt">
+            <div className=" nav nav-pills tabs_events1 d-inline" id="pills-tab">
 
-              <div onClick={() => linkBtn("MYFUNDS")} className="row pt-0 side-nav-link">
-                <div className="col-9 text-start m-0">
-                  <h5 className="m-0 pt-2 ">My Funds</h5>
-                </div>
-                <div className="col-3 text-end m-0">
-                  <p className="m-0 pt-2">→</p>
-                </div>
-                <hr className="mt-3 mb-0" />
+              <div onClick={() => linkBtn("MYFUNDS")} className="d-block pt-0 m-0 side-nav-link nav-link active"
+                data-bs-toggle="pill"
+                aria-controls="pills-home"
+                aria-selected="true"
+              >
+                <h5 className="m-0 pt-2 ">My Funds</h5>
               </div>
 
-              <div onClick={() => linkBtn("MYASSETS")} className="row mt-1 pt-0 side-nav-link">
-                <div className="col-9 text-start m-0">
-                  <h5 className="m-0 pt-2">My Assets</h5>
-                </div>
-                <div className="col-3 text-end m-0">
-                  <p className="m-0 pt-2">→</p>
-                </div>
-                <hr className="mt-3 mb-0" />
+              <div onClick={() => linkBtn("MYASSETS")} className="mt-1 pt-0 side-nav-link nav-link"
+                data-bs-toggle="pill"
+                aria-controls="pills-home"
+                aria-selected="false"
+              >
+                <h5 className="m-0 pt-2">My Assets</h5>
+              </div>
+
+              <div className="mt-1 pt-0 side-nav-link nav-link"
+                data-bs-toggle="pill"
+                aria-controls="pills-home"
+                aria-selected="false"
+              >
+                <h5 className="m-0 pt-2">Other</h5>
+              </div>
+
+              <div className="mt-1 pt-0 side-nav-link nav-link"
+                data-bs-toggle="pill"
+                aria-controls="pills-home"
+                aria-selected="false"
+              >
+                <h5 className="m-0 pt-2">Other</h5>
+              </div>
+
+              <div className="mt-1 pt-0 side-nav-link nav-link"
+                data-bs-toggle="pill"
+                aria-controls="pills-home"
+                aria-selected="false"
+              >
+                <h5 className="m-0 pt-2">Other</h5>
               </div>
 
 
-              <div className="row mt-1 pt-0 side-nav-link">
-                <div className="col-9 text-start m-0">
-                  <h5 className="m-0 pt-2">Other</h5>
-                </div>
-                <div className="col-3 text-end m-0">
-                  <p className="m-0 pt-2">→</p>
-                </div>
-                <hr className="mt-3 mb-0" />
-              </div>
 
-              <div className="row mt-1 pt-0 side-nav-link">
-                <div className="col-9 text-start m-0">
-                  <h5 className="m-0 pt-2">Other</h5>
-                </div>
-                <div className="col-3 text-end m-0">
-                  <p className="m-0 pt-2">→</p>
-                </div>
-                <hr className="mt-3 mb-0" />
-              </div>
 
-              <div className="row mb-3 mt-1 pt-0 side-nav-link">
-                <div className="col-9 text-start m-0">
-                  <h5 className="m-0 pt-2">Other</h5>
-                </div>
-                <div className="col-3 text-end m-0">
-                  <p className="m-0 pt-2">→</p>
-                </div>
-                <hr className="mt-3 mb-0" />
-              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className='px-5 pt-2 col-lg-9 col-md-9 col-12 text-white'>
+      <div className='px-lg-5 pt-2 col-lg-9 col-md-9 col-12 text-white'>
 
         {showdata === "MYFUNDS" ? <MyFunds /> : ""}
         {showdata === "MYASSETS" ? <MyAssets /> : ""}
